3af13b35fcaf12878aee0a613203edf7158e6291
Jeremy Boggs <jeremy@clioweb.org>
2010-06-11 18:59:58 +0000

    Namespaces functions in themes.

--- custom.php
+++ custom.php
@@ -2,11 +2,11 @@
 // Use this file to define customized helper functions, filters or 'hacks' defined
 // specifically for use in your Omeka theme. Note that helper functions that are
 // designed for portability across themes should be grouped into a plugin whenever
-// possible.
+// possible. Ideally, you should namespace these with your theme name.
 
-add_filter(array('Display', 'Item', 'Dublin Core', 'Title'), 'show_untitled_items');
+add_filter(array('Display', 'Item', 'Dublin Core', 'Title'), 'fromscratch_show_untitled_items');
 
-function show_untitled_items($title)
+function fromscratch_show_untitled_items($title)
 {
     // Remove all whitespace and formatting before checking to see if the title 
     // is empty.
