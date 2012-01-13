bdfab8878e4c576bbaed2b5b2527ad50fbf29f59
Jeremy Boggs <jeremy@clioweb.org>
2012-01-13 17:29:03 -0500

    Removes show_untitled_items function and filter..
    The filter for untitled items is already taken care of in
    application/views/scripts/custom.php

--- custom.php
+++ custom.php
@@ -1,18 +1,7 @@
 <?php 
-// Use this file to define customized helper functions, filters or 'hacks' defined
-// specifically for use in your Omeka theme. Note that helper functions that are
-// designed for portability across themes should be grouped into a plugin whenever
-// possible. Ideally, you should namespace these with your theme name.
-
-add_filter(array('Display', 'Item', 'Dublin Core', 'Title'), 'fromscratch_show_untitled_items');
-
-function fromscratch_show_untitled_items($title)
-{
-    // Remove all whitespace and formatting before checking to see if the title 
-    // is empty.
-    $prepTitle = trim(strip_formatting($title));
-    if (empty($prepTitle)) {
-        return '[Untitled]';
-    }
-    return $title;
-}
+/**
+ * Use this file to define customized helper functions, filters or 'hacks'
+ * defined specifically for use in your Omeka theme. Ideally, you should
+ * namespace these with your theme name to avoid conflicts with functions
+ * in Omeka and any plugins.
+ */
