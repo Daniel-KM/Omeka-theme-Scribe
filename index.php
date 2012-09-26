075e8f26ecd7cf351f2b0a25fef2dfb1bba0886b
Matthew Butler <matthewtbutler@gmail.com>
2012-09-26 08:48:39 -0500

    variable cleanup
    changed variables from generic numbers to descriptive collection names
    for easier readability

--- index.php
+++ index.php
@@ -48,78 +48,78 @@
 
     //-------------------Szathmary Culinary Manuscripts and Cookbooks-----------------------------------//
 
-    $col1 = $collectionList[0];
-    $col1_link = link_to_collection($collectionTitle, array(), 'show', $col1);
-    $col1_items = get_items(array('collection' => $col1['id']),9000); 
-    set_items_for_loop($col1_items);
-    $col1_item_list = array();
+    $cookbooks = $collectionList[0];
+    $cookbooks_link = link_to_collection($collectionTitle, array(), 'show', $cookbooks);
+    $cookbooks_items = get_items(array('collection' => $cookbooks['id']),9000); 
+    set_items_for_loop($cookbooks_items);
+    $cookbooks_item_list = array();
        
     while (loop_items()) {
-        $item1 = get_current_item();
-        $col1_idl_link = item('Dublin Core', 'Relation');
-        array_push($col1_item_list, $col1_package=array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
+        get_current_item();
+        $cookbooks_idl_link = item('Dublin Core', 'Relation');
+        array_push($cookbooks_item_list, array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
                                                   'link'=>item_uri(), 'name'=>item('Dublin Core', 'Title')));
     }
 
     //-------------------Nile Kinnick Collection-----------------------------------//
 
-    $col2 = $collectionList[1]; 
-    $col2_link = link_to_collection($collectionTitle, array(), 'show', $col2);
-    $col2_items = get_items(array('collection' => $col2['id']),9000);
-    set_items_for_loop($col2_items);
-    $col2_item_list = array();
+    $kinnick = $collectionList[1]; 
+    $kinnick_link = link_to_collection($collectionTitle, array(), 'show', $kinnick);
+    $kinnick_items = get_items(array('collection' => $kinnick['id']),9000);
+    set_items_for_loop($kinnick_items);
+    $kinnick_item_list = array();
 
     while (loop_items()) {
-        $item2 = get_current_item();
-        $col2_idl_link = item('Dublin Core', 'Relation');
-        array_push($col2_item_list, $col2_package=array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
+        get_current_item();
+        $kinnick_idl_link = item('Dublin Core', 'Relation');
+        array_push($kinnick_item_list, array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
                                                   'link'=>item_uri(), 'name'=>item('Dublin Core', 'Title')));
     }
    
     //-------------------Civil War Diaries and Letters-----------------------------------//
 
-    $col3 = $collectionList[2];
-    $col3_link = link_to_collection($collectionTitle, array(), 'show', $col3);
-    $col3_items = get_items(array('collection' => $col3['id']),9000);
-    set_items_for_loop($col3_items);
-    $col3_item_list = array();
+    $civil_war = $collectionList[2];
+    $civil_war_link = link_to_collection($collectionTitle, array(), 'show', $civil_war);
+    $civil_war_items = get_items(array('collection' => $civil_war['id']),9000);
+    set_items_for_loop($civil_war_items);
+    $civil_war_item_list = array();
 
     while (loop_items()) {
-        $item3 = get_current_item();
-        $col3_idl_link = item('Dublin Core', 'Relation');
-        array_push($col3_item_list, $col3_package=array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
+        get_current_item();
+        $civil_war_idl_link = item('Dublin Core', 'Relation');
+        array_push($civil_war_item_list, array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
                                                   'link'=>item_uri(), 'name'=>item('Dublin Core', 'Title')));
     }
 
     //-------------------Iowa Byington Reed Diaries-----------------------------------//
 
-    $col4 = $collectionList[3];
-    $col4_link = link_to_collection($collectionTitle, array(), 'show', $col4);
-    $col4_items = get_items(array('collection' => $col4['id']),9000);
-    set_items_for_loop($col4_items);
-    $col4_item_list = array();
+    $byington = $collectionList[3];
+    $byington_link = link_to_collection($collectionTitle, array(), 'show', $byington);
+    $byington_items = get_items(array('collection' => $byington['id']),9000);
+    set_items_for_loop($byington_items);
+    $byington_item_list = array();
 
     while (loop_items()) {
-        $item4 = get_current_item();
-        $col4_idl_link = item('Dublin Core', 'Relation');
-        array_push($col4_item_list, $col4_package=array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
+        get_current_item();
+        $byington_idl_link = item('Dublin Core', 'Relation');
+        array_push($byington_item_list, array('thumb'=>item_square_thumbnail(array('alt'=>item('Dublin Core', 'Title'))),
                                                   'link'=>item_uri(), 'name'=>item('Dublin Core', 'Title')));
     }
 
-    $num_of_tr1 = count($col1_items);   
-    $num_of_tr2 = count($col2_items); 
-    $num_of_tr3 = count($col3_items); 
-    $num_of_tr4 = count($col4_items);
+    $num_of_cookbook_items = count($cookbooks_items);   
+    $num_of_kinnick_items = count($kinnick_items); 
+    $num_of_civil_war_items = count($civil_war_items); 
+    $num_of_byington_items = count($byington_items);
 
 ?>
 
-<h1><?php echo $col1_link; ?></h1>
+<h1><?php echo $cookbooks_link; ?></h1>
 <ul id="cookbooks" class="slider">
   <?php
   
-      for ($i=0; $i < $num_of_tr1; $i++) { 
+      for ($i=0; $i < $num_of_cookbook_items; $i++) { 
           echo '<li>';
-          echo '<a href="'.$col1_item_list[$i]['link'].'" rel="tooltip" title="'.$col1_item_list[$i]['name'].'">'.$col1_item_list[$i]['thumb'].'</a>';
+          echo '<a href="'.$cookbooks_item_list[$i]['link'].'" rel="tooltip" title="'.$cookbooks_item_list[$i]['name'].'">'.$cookbooks_item_list[$i]['thumb'].'</a>';
           echo '</li>';
       }
   ?>
@@ -128,13 +128,13 @@
 
 <br />
 
-<h1><?php echo $col4_link; ?></h1>
+<h1><?php echo $byington_link; ?></h1>
 <ul id="byington" class="slider">
   <?php
   
-      for ($i=0; $i < $num_of_tr4; $i++) { 
+      for ($i=0; $i < $num_of_byington_items; $i++) { 
           echo '<li>';
-          echo '<a href="'.$col4_item_list[$i]['link'].'" rel="tooltip" title="'.$col4_item_list[$i]['name'].'">'.$col4_item_list[$i]['thumb'].'</a>';
+          echo '<a href="'.$byington_item_list[$i]['link'].'" rel="tooltip" title="'.$byington_item_list[$i]['name'].'">'.$byington_item_list[$i]['thumb'].'</a>';
           echo '</li>';
       }
   ?>
@@ -143,13 +143,13 @@
 
 <br />
 
-<h1><?php echo $col2_link; ?></h1>
+<h1><?php echo $kinnick_link; ?></h1>
 <ul id="kinnick" class="slider">
   <?php
   
-      for ($i=0; $i < $num_of_tr2; $i++) { 
+      for ($i=0; $i < $num_of_kinnick_items; $i++) { 
           echo '<li>';
-          echo '<a href="'.$col2_item_list[$i]['link'].'" rel="tooltip" title="'.$col2_item_list[$i]['name'].'">'.$col2_item_list[$i]['thumb'].'</a>';
+          echo '<a href="'.$kinnick_item_list[$i]['link'].'" rel="tooltip" title="'.$kinnick_item_list[$i]['name'].'">'.$kinnick_item_list[$i]['thumb'].'</a>';
           echo '</li>';
       }
   ?>
@@ -158,13 +158,13 @@
 
 <br />
 
-<h1><?php echo $col3_link; ?></h1>
+<h1><?php echo $civil_war_link; ?></h1>
 <ul id="civil-war" class="slider">
   <?php
   
-      for ($i=0; $i < $num_of_tr3; $i++) { 
+      for ($i=0; $i < $num_of_civil_war_items; $i++) { 
           echo '<li>';
-          echo '<a href="'.$col3_item_list[$i]['link'].'" rel="tooltip" title="'.$col3_item_list[$i]['name'].'">'.$col3_item_list[$i]['thumb'].'</a>';
+          echo '<a href="'.$civil_war_item_list[$i]['link'].'" rel="tooltip" title="'.$civil_war_item_list[$i]['name'].'">'.$civil_war_item_list[$i]['thumb'].'</a>';
           echo '</li>';
       }
   ?>
